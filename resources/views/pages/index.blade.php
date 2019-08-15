439890fc7024243719b6fb8e58cc337f5259bb3b
Finn Armstrong-McAllister (1000050934) <ARMSFA1@student.op.ac.nz>
2019-08-15 16:10:57 +1200

    Hand in my laravel tutorial

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('content')
+    <h2 class="mt-5 mb-3 text-center">Laravel 5.8 for Beginniners!</h2>
+    <p class="text-center">fodpgkfdkgpodfkgpdfogkdfpkgodfkpgdf</p>
+@endsection
